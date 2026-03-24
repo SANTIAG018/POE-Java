cb2ecb307da47efd54fc0bef8aa2a707d12f1148
SANTIAG018 <kevin.palomino@correounivalle.edu.co>
2026-03-24 00:11:48 -0500

    corregi codigo mostruo

---
+++ src/Monstruo.java
@@ -0,0 +1,13 @@
+public class Monstruo extends Carta {
+
+    public Monstruo() {
+    }
+
+    public Monstruo(String nombre, short ataque, short defensa, Estrellas estrellas, boolean visible) {
+        super(nombre, ataque, defensa, TipoCarta.MONSTRUO, estrellas, visible);
+    }
+
+    public void atacar() {
+        System.out.println(getNombre() + " ataca con " + getAtaque() + " puntos!");
+    }
+}
