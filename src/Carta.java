3b4a3e629cf4f5b81d670558965826c124cede58
SANTIAG018 <kevin.palomino@correounivalle.edu.co>
2026-04-07 00:03:00 -0500

    rehago clase carta por error en la estructura inicial

--- src/Carta.java
+++ src/Carta.java
@@ -1,80 +1,26 @@
-public class Carta {
-    public abstract class Carta {
+public abstract class Carta {
 
-    private String nombre;
-    private short ataque;
-    private short defensa;
-    private TipoCarta tipo;
-    private Estrellas estrellas;
-    private boolean visible;
+    private String nombreCarta;
+    private TipoCarta tipoCarta;
+    private boolean estaVisible;
 
-    public Carta() {
+    public Carta(String nombreCarta, TipoCarta tipoCarta, boolean estaVisible) {
+        this.nombreCarta = nombreCarta;
+        this.tipoCarta = tipoCarta;
+        this.estaVisible = estaVisible;
     }
 
-    public Carta(String nombre, short ataque,
-        short defensa, TipoCarta tipo, Estrellas estrellas,
-        boolean visible) {
-
-        this.nombre = nombre;
-        this.ataque = ataque;
-        this.defensa = defensa;
-        this.tipo = tipo;
-        this.estrellas = estrellas;
-        this.visible = visible;
+    public String getNombreCarta() {
+        return nombreCarta;
     }
 
-    public String getNombre() {
-        return nombre;
+    public TipoCarta getTipoCarta() {
+        return tipoCarta;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public boolean isEstaVisible() {
+        return estaVisible;
     }
 
-    public short getAtaque() {
-        return ataque;
-    }
-
-    public void setAtaque(short ataque) {
-        this.ataque = ataque;
-    }
-
-    public short getDefensa() {
-        return defensa;
-    }
-
-    public void setDefensa(short defensa) {
-        this.defensa = defensa;
-    }
-
-    public TipoCarta getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoCarta tipo) {
-        this.tipo = tipo;
-    }
-
-    public Estrellas getEstrellas() {
-        return estrellas;
-    }
-
-    public void setEstrellas(Estrellas estrellas) {
-        this.estrellas = estrellas;
-    }
-
-    public boolean isVisible() {
-        return visible;
-    }
-
-    public void setVisible(boolean visible) {
-        this.visible = visible;
-    }
-
-    public enum Estrellas {
-        UNA, DOS, TRES, CUATRO, CINCO,
-        SEIS, SIETE, OCHO, NUEVE, DIEZ,
-        ONCE, DOCE
-    }
-}
+    public abstract void usar(Jugador jugadorActual, Jugador jugadorRival);
 }
